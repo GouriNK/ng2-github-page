ec7238f82e52a2d0e8a8ef420e0d94b52b5ffaaa
GouriNK <gouri.nandakumar26@gmail.com>
2017-03-19 20:55:37 +0100

    added material design and material layout support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'this is a sample app to test angular 2 app deployment on github pages!';
+
+  selectedValue: string;
+
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 }
